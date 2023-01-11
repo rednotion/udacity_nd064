3074e5094c14b2524d7f505143e5496b2ccc5500
elizabeth-dsaid <elizabeth@dsaid.gov.sg>
2023-01-11 18:18:25 +0800

    app changes for loggign

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,19 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+from datetime import datetime
+import logging
+
+# Set logging config
+logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    #n_connections += 1
     return connection
 
 # Function to get a post using its ID
@@ -16,6 +23,16 @@ def get_post(post_id):
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
+
+    # log if post
+    timestamp = datetime.now()
+    if post:
+        log = f"{timestamp}: Article '{post['Title']}' retrieved"
+    else:
+        log = f"{timestamp}: Article not found, 404 returned"
+    logging.info(log)
+    print(log)
+
     return post
 
 # Define the Flask application
@@ -43,6 +60,11 @@ def post(post_id):
 # Define the About Us page
 @app.route('/about')
 def about():
+    timestamp = datetime.now()
+    log = f"{timestamp}: 'About Us' page was retrieved"
+    logging.info(log)
+    print(log)
+
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,10 +83,48 @@ def create():
             connection.commit()
             connection.close()
 
+            timestamp = datetime.now()
+            log = f"{timestamp}: New article with title '{title}' was created"
+            logging.info(log)
+            print(log)
+
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Healthcheck endpoint
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps("result: OK - healthy"),
+        status=200,
+        mimetype="application/json"
+    )
+    logging.info("Status 200 OK")
+
+    return response
+
+
+# Metrics endpoint
+@app.route('/metrics')
+def metrics():
+    
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    metric_body = {
+        "db_connection_count": n_connections,
+        "post_count": len(posts), 
+    }
+    response = app.response_class(
+        response=json.dumps(metric_body),
+        status=200,
+        mimetype="application/json"
+    )
+
+    return response
+
+
 # start the application on port 3111
 if __name__ == "__main__":
+   #n_connections=0
    app.run(host='0.0.0.0', port='3111')
