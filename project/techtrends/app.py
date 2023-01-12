4f0dbe1c5674705779315f7bf04f0c531e2ddf44
elizabeth-dsaid <elizabeth@dsaid.gov.sg>
2023-01-12 16:21:20 +0800

    standout 1 and 3

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -99,6 +99,27 @@ def create():
 # Healthcheck endpoint
 @app.route('/healthz')
 def healthcheck():
+    # 500 error
+    error_response = app.response_class(
+        response=json.dumps("result: ERROR - unhealthy"),
+        status=500,
+        mimetype="application/json"
+    )
+
+    # check if connection to DB fails
+    try: 
+        connection = sqlite3.connect('database.db')
+    except:
+        logging.info("Status 500 ERROR")
+        return error_response
+
+    # check if posts db exists
+    try:
+        posts = connection.execute('SELECT * FROM posts').fetchall()
+    except:
+        logging.info("Status 500 ERROR")
+        return error_response
+
     response = app.response_class(
         response=json.dumps("result: OK - healthy"),
         status=200,
